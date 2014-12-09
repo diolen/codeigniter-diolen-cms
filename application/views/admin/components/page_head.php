f5636d7c2783550c3a9d2f20dddd186695c81d3b
Oleg <oleg.urazbayev@gmail.com>
2014-12-09 01:11:06 -0300

    Admin panel - managing pages

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -6,16 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="<?php echo site_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
-        <link href="<?php echo site_url('css/admin.css'); ?>" rel="stylesheet">
         
         <script src="<?php echo site_url('js/jquery.js'); ?>"></script>
         <script src="<?php echo site_url('js/bootstrap.min.js'); ?>"></script>
-        <?php if(isset($sortable) && $sortable === TRUE): ?>
-        <link href="<?php echo site_url('css/jquery-ui.min.css'); ?>" rel="stylesheet">
-        <script src="<?php echo site_url('js/jquery-ui.min.js'); ?>"></script>
-        <script src="<?php echo site_url('js/jquery.mjs.nestedSortable.js'); ?>"></script>
-        <?php endif; ?>
-
+        
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- TinyMCE -->
         <script type="text/javascript" src="<?php echo site_url('js/tinymce/tinymce.min.js'); ?>"></script>
