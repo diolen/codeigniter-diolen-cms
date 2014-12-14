1013acfd515494f1f5d6bf6e5d4b0a23c645e742
Oleg <oleg.urazbayev@gmail.com>
2014-12-14 15:52:26 -0300

    Start Frontent

---
+++ application/views/components/page_head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo config_item('site_name') ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="<?php echo site_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
+        
+        <script src="<?php echo site_url('js/jquery.js'); ?>"></script>
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    </head>        
